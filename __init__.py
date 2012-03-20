d097954c811a84cc13867a0a21acc55b666eb141
gardnerj <gardnerj@phys.washington.edu>
2012-03-20 20:46:11 +0000

    Make this a Python package by adding __init__.py

---
+++ __init__.py
@@ -0,0 +1,23 @@
+"""
+LSST Image Simulation Software
+Copyright 2008, 2009, 2010, 2011, 2012 LSST Corporation.
+
+This product includes software developed by the
+LSST Project (http://www.lsst.org/) with contributions made at LSST partner
+institutions:
+
+  * The LSST Corporation
+  * Purdue University
+  * The University of California, Berkeley
+  * The Stanford Linear Accelerator Laboratory (SLAC)
+  * University of Washington
+
+Use and redistribution of this software is covered by the GNU Public
+License Version 3 (GPLv3) or later, as detailed below.  A copy of the
+GPLv3 is also available at <http://www.gnu.org/licenses/>.
+"""
+__author__ = "Jeffrey P. Gardner <jeffpg@uw.edu>"
+__license__ = "GPL v3"
+__all__ = [ "AllVisitsScriptGenerator", "SingleVisitScriptGenerator",
+            "AllChipsScriptGenerator", "SingleChipScriptGenerator",
+            "AbstractScriptGenerator", "chip.py"]
